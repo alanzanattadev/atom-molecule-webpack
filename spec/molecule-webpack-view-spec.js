f6210f1bf1c3d7d818847d522ed917ae20f1db8f
Alan <alan.zanatta@outlook.com>
2016-07-30 02:13:56 +0200

    Adds base (Diagnostics, TaskBar, Status, Progression)

---
+++ spec/molecule-webpack-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MoleculeWebpackView from '../lib/molecule-webpack-view';
+
+describe('MoleculeWebpackView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
